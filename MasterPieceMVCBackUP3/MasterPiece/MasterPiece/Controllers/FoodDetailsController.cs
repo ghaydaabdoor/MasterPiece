153f68d5bc9423b0f512c86dc378fd3f7cf8ff80
ghaydaabdoor <ghaydaa.bdoor@outlook.com>
2024-09-23 01:29:18 +0300

    back up master piece

---
+++ MasterPieceMVCBackUP3/MasterPiece/MasterPiece/Controllers/FoodDetailsController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MasterPiece.Models;
+
+namespace MasterPiece.Controllers
+{
+    public class FoodDetailsController : Controller
+    {
+        private MasterPieceEntities1 db = new MasterPieceEntities1();
+
+        // GET: FoodDetails
+        public ActionResult Index(int? id)
+        {
+            var foodID = db.Foods.Find(id);
+            var foodDetailsID = db.FoodDetails.FirstOrDefault(p => p.FoodId == id);
+
+            var viewModel = new FoodViewModel
+            {
+                food = foodID,
+                foodDetail = foodDetailsID,
+            };
+
+            return View(viewModel);
+        }
+
+
+        // GET: FoodDetails/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            FoodDetail foodDetail = db.FoodDetails.Find(id);
+            if (foodDetail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(foodDetail);
+        }
+
+        // GET: FoodDetails/Create
+        public ActionResult Create()
+        {
+            ViewBag.FoodId = new SelectList(db.Foods, "FoodId", "Name");
+            return View();
+        }
+
+        // POST: FoodDetails/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "FoodDetailId,FoodId,ImageUrl1,ImageUrl2,ImageUrl3,Description,PriceDetails")] FoodDetail foodDetail)
+        {
+            if (ModelState.IsValid)
+            {
+                db.FoodDetails.Add(foodDetail);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.FoodId = new SelectList(db.Foods, "FoodId", "Name", foodDetail.FoodId);
+            return View(foodDetail);
+        }
+
+        // GET: FoodDetails/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            FoodDetail foodDetail = db.FoodDetails.Find(id);
+            if (foodDetail == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.FoodId = new SelectList(db.Foods, "FoodId", "Name", foodDetail.FoodId);
+            return View(foodDetail);
+        }
+
+        // POST: FoodDetails/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "FoodDetailId,FoodId,ImageUrl1,ImageUrl2,ImageUrl3,Description,PriceDetails")] FoodDetail foodDetail)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(foodDetail).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.FoodId = new SelectList(db.Foods, "FoodId", "Name", foodDetail.FoodId);
+            return View(foodDetail);
+        }
+
+        // GET: FoodDetails/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            FoodDetail foodDetail = db.FoodDetails.Find(id);
+            if (foodDetail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(foodDetail);
+        }
+
+        // POST: FoodDetails/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            FoodDetail foodDetail = db.FoodDetails.Find(id);
+            db.FoodDetails.Remove(foodDetail);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MasterPieceMVCBackUP3/MasterPiece/MasterPiece/Controllers/UsersController.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MasterPiece.Models;
+
+namespace MasterPiece.Controllers
+{
+    public class UsersController : Controller
+    {
+        private MasterPieceEntities1 db = new MasterPieceEntities1();
+
+        // GET: Users
+        public ActionResult Index()
+        {
+            return View(db.Users.ToList());
+        }
+
+        //// GET: Users/Details/5
+        //public ActionResult Details(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    User user = db.Users.Find(id);
+        //    if (user == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(user);
+        //}
+
+        // GET: Users/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Users/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "FirstName,LastName,Email,Password")] User user)
+        {
+            if (db.Users.Any(p => p.Email == user.Email))
+            {
+                ModelState.AddModelError("Email", " البريد الإلكتروني موجود بالفعل. يرجى استخدام بريد إلكتروني آخر");
+            }
+            if (ModelState.IsValid)
+            {
+                db.Users.Add(user);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(user);
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(User model)
+        {
+            // Fetch the user by email
+            var user = db.Users.FirstOrDefault(p => p.Email == model.Email);
+
+            // Check if user exists
+            if (user == null)
+            {
+                ModelState.AddModelError("Email", "البريد الالكتروني غير صحيح");
+            }
+            else
+            {
+                if (model.Password != user.Password) 
+                {
+                    ModelState.AddModelError("Password", "كلمة السر غير صحيحة");
+                }
+            }
+
+            if (ModelState.IsValid)
+            {
+                Session.Clear();
+                Session["UserId"] = user.UserId;
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View(model);
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult Profile()
+        {
+            var userId = Session["UserId"];
+            if (userId == null)
+            {
+                return RedirectToAction("Login", "Users");
+            }
+
+            var user = db.Users.FirstOrDefault(p => p.UserId == (int)userId);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(user);
+        }
+
+        public ActionResult EditProfile(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(user);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditProfile([Bind(Include = "UserId,FirstName,LastName,Phone,Address")] User model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Fetch the existing user to preserve fields not in the form (e.g., Email, Password)
+                var existingUser = db.Users.Find(model.UserId);
+                if (existingUser != null)
+                {
+                    // Update only the editable fields
+                    existingUser.FirstName = model.FirstName;
+                    existingUser.LastName = model.LastName;
+                    existingUser.Phone = model.Phone;
+                    existingUser.Address = model.Address;
+
+                    // Preserve non-editable fields like Email and Password
+                    // db.Entry(existingUser).State = EntityState.Modified;  // No need for this, you are directly updating fields
+
+                    db.SaveChanges();
+                    return RedirectToAction("Profile");
+                }
+            }
+
+            return View(model);
+        }
+
+
+        // GET: Users/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // POST: Users/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            User user = db.Users.Find(id);
+            db.Users.Remove(user);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
